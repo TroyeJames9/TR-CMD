87774c32011399dacf9e2cef7c7cf875b1611a5b
troyejames9 <130838331+TroyeJames9@users.noreply.github.com>
2025-07-25 19:31:30 +0800

    update_helpCMD
    Signed-off-by: troyejames9 <130838331+TroyeJames9@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -9,23 +9,27 @@ Created on Wed Apr  4 19:06:08 2018
 import subprocess
 import configparser
 import os
+# noinspection PyPackageRequirements
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+# noinspection PyPackageRequirements
+from telegram import ParseMode
 import logging
-
 
 config = configparser.ConfigParser()
 config.read("config")
-### Get admin chat_id from config file
-### For more security replies only send to admin chat_id
+# Get admin chat_id from config file
+# For more security replies only send to admin chat_id
 adminCID = config["SecretConfig"]["admincid"]
 
-### Enable logging
+# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
-### This function run command and send output to user
+
+
+# This function run command and send output to user
 def runCMD(bot, update):
     if not isAdmin(bot, update):
         return
@@ -40,24 +44,11 @@ def runCMD(bot, update):
         bot.sendMessage(text=str(cmdErr, "utf-8"), chat_id=adminCID)
 
 
-### This function ping 8.8.8.8 and send you result
-def ping8(bot, update):
-    if not isAdmin(bot, update):
-        return
-    cmdOut = str(
-        subprocess.check_output(
-            "ping", "8.8.8.8 -c4", stderr=subprocess.STDOUT, shell=True
-        ),
-        "utf-8",
-    )
-    bot.sendMessage(text=cmdOut, chat_id=adminCID)
-
-
 def startCMD(bot, update):
     if not isAdmin(bot, update):
         return
     bot.sendMessage(
-        text="Welcome to TSMB bot, this is Linux server/PC manager, Please use /help and read carefully!!",
+        text="这里是tflowbot，你的服务器的私人助手, 请执行/help以浏览我所能提供的服务",
         chat_id=adminCID,
     )
 
@@ -65,10 +56,15 @@ def startCMD(bot, update):
 def helpCMD(bot, update):
     if not isAdmin(bot, update):
         return
-    bot.sendMessage(
-        text="This bot has access to your server/PC, So it can do anything. Please use Telegram local password to prevent others from accessing to this bot.",
-        chat_id=adminCID,
-    )
+
+    # 获取脚本所在目录的绝对路径
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    help_file = os.path.join(script_dir, "assets", "help.md")
+
+    with open(help_file, "r", encoding="utf-8") as file:
+        help_text = file.read()
+
+    bot.sendMessage(text=help_text, chat_id=adminCID, parse_mode=ParseMode.MARKDOWN)
 
 
 def topCMD(bot, update):
@@ -80,10 +76,10 @@ def topCMD(bot, update):
 
 
 def HTopCMD(bot, update):
-    ## Is this user admin?
+    # Is this user admin?
     if not isAdmin(bot, update):
         return
-    ## Checking requirements on your system
+    # Checking requirements on your system
     htopCheck = subprocess.call(["which", "htop"])
     if htopCheck != 0:
         bot.sendMessage(
@@ -105,7 +101,7 @@ def HTopCMD(bot, update):
         os.remove("./htop-output.html")
 
 
-def error(bot, update, error):
+def error(update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
@@ -114,7 +110,7 @@ def isAdmin(bot, update):
     chat_id = update.message.chat_id
     if str(chat_id) == adminCID:
         return True
-    
+
     update.message.reply_text(
         "You cannot use this bot, because you are not Admin!!!!"
     )
@@ -136,7 +132,6 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", startCMD))
-    dp.add_handler(CommandHandler("ping8", ping8))
     dp.add_handler(CommandHandler("top", topCMD))
     dp.add_handler(CommandHandler("htop", HTopCMD))
     dp.add_handler(CommandHandler("help", helpCMD))
